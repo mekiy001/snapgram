27f3a43ce33d9856146e2249cc3d0bd8dfd52613
Isse <isse1@live.com.au>
2024-12-16 17:41:58 +1030

    Implement top bar

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import TopBar from "../components/shared/Topbar";
+import LeftSidebar from "../components/shared/LeftSidebar";
+import { Outlet } from "react-router-dom";
+import Bottombar from "../components/shared/Bottombar";
 
 function RootLayout() {
     return(
-        <div>RootLayout</div>
+        <div className="w-full md:flex">
+            <TopBar />
+            <LeftSidebar />
+            <section className="flex flex-1 h-full">
+                <Outlet />
+            </section>
+            <Bottombar />
+        </div>
     );
 }
 
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -86,3 +86,13 @@ export async function getCurrentUser() {
         console.log(error);
     }
 }
+
+export async function signOutAccount() {
+    try {
+        const session = await account.deleteSession("current");
+
+        return session;
+    } catch(err) {
+        console.log(err);
+    }
+}
